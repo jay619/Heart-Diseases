088a32a35f0744f4989cb6e982cfaec11a0311b1
Jaynish Shah <shahjaynish@gmail.com>
2022-12-03 20:13:29 +0530

    streamlit test

--- src/heart_diseases.py
+++ src/heart_diseases.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # import scipy.stats as stats
 # from sklearn.model_selection import train_test_split
-# import seaborn as sns
+import seaborn as sns
 import streamlit as st
 
 FILE_PATH_DATA = 'input/heart_2020_cleaned.csv'
