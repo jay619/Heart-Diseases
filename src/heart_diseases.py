a0a342e7c11969519361ded7a428a2487bdcafc4
Jaynish Shah <shahjaynish@gmail.com>
2022-12-03 20:12:32 +0530

    test

--- src/heart_diseases.py
+++ src/heart_diseases.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
 # import scipy.stats as stats
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 # import seaborn as sns
 import streamlit as st
 
