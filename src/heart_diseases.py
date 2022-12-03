785e8b6898ebc92c332c40ceda6877c9b54c6f28
Jaynish Shah <shahjaynish@gmail.com>
2022-12-03 15:26:10 +0530

    Created source file

---
+++ src/heart_diseases.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# import scipy.stats as stats
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import streamlit as st
+
+FILE_PATH_DATA = 'input/heart_2020_cleaned.csv'
+
+def main():
+    st.title("Heart Diseases")
+
+if __name__ == '__main__':
+    main()
