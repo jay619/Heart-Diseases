8a8b6891a591343a2f1ee334cc453263162f94df
Jaynish Shah <shahjaynish@gmail.com>
2022-12-04 00:20:17 +0530

    Added introduction

--- src/Hello.py
+++ src/Hello.py
@@ -17,6 +17,13 @@ def main():
         page_icon='heart'
     )
     st.markdown("# Heart Diseases")
+    st.markdown("## Introduction")
+    st.markdown("""**Center for Disease Control and Prevention (CDC)** states that cardiovascular diseases (CVDs) are one of the leading causes of death in the US as well as globally. Heart diseases lead to about 697,000 deaths in the United States for the year 2020 – which is 1 in every 5th death (1). CVDs cost the country a lot of money and resources and it includes cost such health care services, medicines and loss of productivity due to death. About half of all Americans (47%) have at least 1 of 3 key risk factors for heart disease: high blood pressure, high cholesterol, and smoking.""", unsafe_allow_html=False)
+    st.markdown("""
+    How to navigate this web application:
+    - To look at the dashboard, select the **Dashboard** page on the left
+    - To view/use the predictive model, select the **Predictive Model** page on the left
+    """)
 
     @st.cache(persist=True)
     def load_data():
