ec1a8abd63eecddae76137941fe3aeeb85bd3bc6
Jaynish Shah <shahjaynish@gmail.com>
2022-12-03 21:38:01 +0530

    Streamlit initial commit

--- src/heart_diseases.py
+++ src/heart_diseases.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import scipy.stats as stats
+import scipy.stats as stats
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import streamlit as st
@@ -9,7 +9,18 @@ import streamlit as st
 FILE_PATH_DATA = 'input/heart_2020_cleaned.csv'
 
 def main():
-    st.title("Heart Diseases")
+    st.set_page_config(
+        page_title="Heart Diseases",
+        layout="wide",
+        initial_sidebar_state="auto"
+    )
+
+    @st.cache(persist=True)
+    def load_data():
+        data = pd.read_csv(FILE_PATH_DATA)
+        return data
+
+    df = load_data()
 
 if __name__ == '__main__':
     main()
