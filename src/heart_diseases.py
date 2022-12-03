3ab0f31c679f7db53fe29a3ea79c29f872eda164
Jaynish Shah <shahjaynish@gmail.com>
2022-12-03 19:29:41 +0530

    Requirements.txt file for dependencies

--- src/heart_diseases.py
+++ src/heart_diseases.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # import scipy.stats as stats
 from sklearn.model_selection import train_test_split
 # import seaborn as sns
