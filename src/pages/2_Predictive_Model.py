d048ea5e36fc31131a5d8f2c3c54d3f57801345a
Jaynish Shah <shahjaynish@gmail.com>
2022-12-04 20:26:18 +0530

    Confusion matrix & metrics

--- src/pages/2_Predictive_Model.py
+++ src/pages/2_Predictive_Model.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 FILE_PATH_DATA = 'input/heart_2020_cleaned.csv'
 
@@ -21,7 +24,11 @@ def main():
     df.columns = df.columns.str.lower()
 
     # Encoding target variable
-    df['heartdisease'].replace({'Yes':1,'No':0},inplace=True)
+    # df['heartdisease'].replace({'Yes':1,'No':0},inplace=True)
+
+    label_encoder = LabelEncoder()
+    label_encoder.fit_transform(df['heartdisease'])
+
 
     # Spliting target and feature variables
     target = df['heartdisease']
@@ -57,10 +64,34 @@ def main():
     clf = LogisticRegression(max_iter=500, class_weight='balanced')
     clf.fit(X_train_encoded, y_train)
 
-    # Model Prediction
+    ### Transform Test Data
+    test_scaled = minmax.transform(X_test[['bmi', 'physicalhealth', 'mentalhealth', 'sleeptime']])
+    test_scaled_df = pd.DataFrame(test_scaled, columns=['bmi', 'physicalhealth', 'mentalhealth', 'sleeptime'])
+    
+    test_encoded = ordinal_enc.transform(X_test[['smoking', 'alcoholdrinking', 'stroke', 'diffwalking', 'physicalactivity', 'asthma', 'kidneydisease', 'skincancer']])
+    test_ordinal_enc_df = pd.DataFrame(test_encoded, columns=ordinal_enc.feature_names_in_)
 
+    test_one_hot_encoded = one_hot.transform(X_test[['sex', 'agecategory', 'race', 'diabetic', 'genhealth']]).toarray()
+    test_one_hot_encoded_df = pd.DataFrame(test_one_hot_encoded, columns=one_hot.get_feature_names_out())
+
+    X_test_encoded = pd.concat([test_ordinal_enc_df, test_scaled_df, test_one_hot_encoded_df], axis=1)
+
+    y_pred = clf.predict(X_test_encoded)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("##### Model Accuracy: `{:.2%}`\tModel Precision: `{:.2%}`".format(accuracy_score(y_true=y_test, y_pred=y_pred), precision_score(y_true=y_test, y_pred=y_pred, pos_label="Yes")))
+        st.markdown("##### Model Recall: `{:.2%}`\tModel F1-Score: `{:.2%}`".format(recall_score(y_true=y_test, y_pred=y_pred, pos_label="Yes"), f1_score(y_true=y_test, y_pred=y_pred, pos_label="Yes")))
+
+    with col2:
+        fig, ax = plt.subplots(figsize=(3,3))
+        cm = confusion_matrix(y_true=y_test, y_pred=y_pred)
+        sns.heatmap(cm, annot=True, fmt="g", ax=ax)
+        st.pyplot(fig=fig)
+
+
+    # Model Prediction
     st.subheader("Patient Details")
-    # is_predict = st.sidebar.button(label="Predict")
 
     with st.form(key="patient_details", clear_on_submit=False):
         c1, c2 = st.columns(2)
@@ -93,9 +124,14 @@ def main():
             user_inp_one_hot = one_hot.transform([[user_sex, user_age_cat, user_race, user_diabetic, user_gen_health]]).toarray()
 
             user_input = np.concatenate([user_inp_scaled, user_inp_ordinal, user_inp_one_hot], axis=1)
-            st.write(clf.predict(user_input))
-            # st.write(user_inp_ordinal)
-            # st.write(user_inp_one_hot)
+            user_inp_pred = clf.predict(user_input)[0]
+    
+    
+    if user_inp_pred == "Yes":
+        st.markdown("### Does user have a high chance of heart diseases? Based on the inputs, we think: `{}` 🚨".format(user_inp_pred))
+    else:
+        st.markdown("### Does user have a high chance of heart diseases? Based on the inputs, we think: `{}` ✅".format(user_inp_pred))
+            # label_encoder.inverse_transform(user_inp_pred)[0]
 
     
 
